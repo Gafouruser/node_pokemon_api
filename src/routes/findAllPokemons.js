e7d66063892df85f05c281816ea151b594cff32e
Gafouruser <yekiniabdougafour@gmail.com>
2024-12-16 15:09:09 +0100

    error manage end

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -1,15 +1,40 @@
-const { Pokemon } = require('../db/sequelize')
+const { Pokemon } = require("../db/sequelize");
+const { Op } = require("sequelize");
 
 module.exports = (app) => {
-    app.get('/api/pokemons', (req, res) => {
-        Pokemon.findAll()
-        .then(pokemons => {
-            const message = 'La liste des pokemons a bien ete recuperee.'
-            res.json({ message, data: pokemons })
+  app.get("/api/pokemons", (req, res) => {
+    if (req.query.name) {
+      const name = req.query.name
+      const limit = parseInt(req.query.limit) || 5
+
+      if(name.length < 2) {
+        const message = 'Le terme de recherche doit contenir au moins 2 caracteres.'
+        return res.status(400).json({ message })
+      }
+
+      return Pokemon.findAndCountAll({
+        where: {
+          name: {
+            [Op.like]: `%${name}%`,
+          },
+        },
+        order: ['name'],
+        limit: limit
+      }).then(({count, rows}) => {
+        const message = `Il y a ${count} pokemons qui correspondent au terme de recherche ${name}.`;
+        res.json({ message, data: rows });
+      });
+    } else {
+      Pokemon.findAll({ order: ['name'] })
+        .then((pokemons) => {
+          const message = "La liste des pokemons a bien ete recuperee.";
+          res.json({ message, data: pokemons });
         })
-        .catch(error => {
-            const message = 'La liste des pokemons n\'a pas pu etre recuperee. Reesayer dans quelques instants.'
-            res.status(500).json({ message, data: error })
-        })
-    })
-}
+        .catch((error) => {
+          const message =
+            "La liste des pokemons n'a pas pu etre recuperee. Reesayer dans quelques instants.";
+          res.status(500).json({ message, data: error });
+        });
+    }
+  });
+};
